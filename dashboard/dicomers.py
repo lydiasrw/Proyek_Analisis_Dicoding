c6b18694a38414db0a7e32048d6d398d7f3ab72b
Lydia Saraswati <121739227+lydiasrw@users.noreply.github.com>
2023-12-06 15:07:39 +0700

    Update dicomers.py

--- dashboard/dicomers.py
+++ dashboard/dicomers.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 import streamlit as st
 from babel.numbers import format_currency
-from PIL import GifImagePlugin, Image, ImageFile
 
 sn.set(style="dark")
 
